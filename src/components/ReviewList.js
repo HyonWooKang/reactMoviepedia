eb87e5fb93ce0fdc6f5526e7d09ebb2ecf5982ab
HyonWooKang <87benzenes@gmail.com>
2023-12-05 19:11:39 +0900

    상세 아이템이 화면에 보이도록 구현

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,6 +1,30 @@
+function ReviewListItem({ item }) {
+  return (
+    <div className="ReviewList">
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{item.createdAt}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
+
 function ReviewList({ items }) {
   console.log(items);
-  return <ul></ul>;
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li>
+            <ReviewListItem item={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default ReviewList;
