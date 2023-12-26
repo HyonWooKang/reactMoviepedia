f5d8013d18a10aa7535dcb8c1f33a55419197a9e
HyonWooKang <87benzenes@gmail.com>
2023-12-26 19:09:12 +0900

    파일 등록 초기화 추가

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -2,22 +2,30 @@ import { useEffect, useRef, useState } from "react";
 
 function FileInput({ name, value, onChange }) {
   // const [value, setValue] = useState();
-  const inputRef = useRef();
+  const [preview, setPreview] = useState(); // 미리보기
+  const inputRef = useRef(); // 사진 초기화용 // null check 하여 사용하기
 
-  // 랜더링 할 때 한 번 사용
-  // useEffect(() => {
-  //   // ref 값이 있을지 모르니 예외 처리
-  //   if (inputRef.current) {
-  //     console.log(inputRef.current);
-  //   }
-  // }, []);
-
+  // 파일 등록
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} ref={inputRef} />;
+  // 파일 초기화
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null); // 이미지 파일의 value가 null로 처리됨
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
 // input file은 비제어 input으로 value를 바인딩 하면 안 됨 (value={value} => error 발생)
 // js로 file의 값(값, 경로 등)을 바꿀 수 없음 (보안적이 이유)
